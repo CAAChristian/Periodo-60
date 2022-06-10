a3c9df44b9f8d7a6cbf4c68abe560be0e4311bf9
CAAChristian <104937676+CAAChristian@users.noreply.github.com>
2022-06-10 08:54:32 -0500

    Add files via upload

---
+++ kew.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 07:32:48 2022
+
+@author: Alumno
+"""
+
+def wishes():
+    print("My Wishes")
+    return "Happy Birthday"
+
+print(wishes())
+
+   
