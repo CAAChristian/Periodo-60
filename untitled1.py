685069bd1d57a3a37286571c9a8923b7687b0381
CAAChristian <104937676+CAAChristian@users.noreply.github.com>
2022-06-10 08:57:05 -0500

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 07:47:17 2022
+
+@author: Alumno
+"""
+
+def saludo(miLista):
+    for item in miLista:
+        print("Hola! ",item)
+    print(" ")
+        
+saludo(["Juan"])
+saludo(["Juan","Ana"])
+saludo(["Juan","Ana","Dillan"])
