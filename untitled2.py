fe716e3b7a9bc5e5612f2cfc834670d4273228fb
CAAChristian <104937676+CAAChristian@users.noreply.github.com>
2022-06-10 08:57:31 -0500

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 07:53:18 2022
+
+@author: Alumno
+"""
+
+def creoLista(n):
+    Lista=[]
+    for i in range(1,n):
+        Lista.append(i)
+    return Lista
+print(creoLista(101))
