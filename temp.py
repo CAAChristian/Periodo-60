414c7de4acbc901083c06965c7a7724b2b9f06bd
CAAChristian <104937676+CAAChristian@users.noreply.github.com>
2022-06-10 08:58:08 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+@author: juan carlos
+"""
+hora=int(input("Ingrese una hora: "))
+minutos=int(input("Ingrese un minuto: "))
+segundos=int(input("Ingrese los segundos: "))
+agregaciondesegundos=int(input("Ingrese los segundos que quiera sumar: "))
+if segundos>=minutos:
+    int("se suman estos resultados: ")
+    minutostotales=segundos+minutos 
+if segundos>=59:
+    int("error ingrese nuevamente los segundos: ")
+if segundos<=-1:
+    int("error ingrese nuevamente los segundos positivas: ")
+if minutos>=horas:
+    int("se suman los resultados: ")
+    horastotales=minutos mas horas
+if minutos>=59:
+    int("error ingrese nuevamente los minutos: ")
+if minutos<=-1:
+    int("ingrese nuevamente los minutos positivas: ")
+if hora>=23:
+    int("error ingrese nuevamente las horas: ")
+if hora<=-1:
+    int("error ingrese nuevamente las horas positivas: ")
+minutostotales=segundos+agregaciondesegundos
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+  
